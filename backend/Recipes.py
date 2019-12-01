031cfb26fe78a64918c6817e9c6bce05853e961b
Pernille <pernille.fathi@kcl.ac.uk>
2019-12-01 01:39:06 +0000

    Update Recipes.py
    New stuff ;)

---
+++ backend/Recipes.py
@@ -0,0 +1,20 @@
+
+potatoTortilla = ["potatoes", "eggs", "onions"] #salt, pepper
+
+breadSoup = ["bread", "onions","frozen peas"] #salt, pepper, stock cube, oil
+
+cauliflowerSoup = ["onions", "garlic", "cauliflower", "kale", "bacon", "milk"] #stock cube, salt, pepper, oil
+
+chickenSalad = ["chicken", "salad", "red onions", "cucumber", "pommegranate", "salad", "avocado", "feta cheese"] # salt, pepper, oil/butter
+
+bananaPancakes = ["milk", "butter", "eggs", "bananas"] # flour, baking powder, salt
+
+frenchToast = ["bread", "eggs", "milk"] # oil, sugar, salt
+
+breadPizza = ["bread", "tomatoes", "cheese"] # pepper, herbs
+
+
+# [recipe, name of recipe, time, dietaryRestrictions]
+recipes = [[bananaPancakes, "Banana Pancakes", 15, "Ve"], [breadSoup, "Bread Soup", 30, "V"],[cauliflowerSoup, "Cauliflower Soup", 30, "G"] , [chickenSalad, "Chicken Salad", 15, "G"], [frenchToast, "French Toast", 10, "Ve"], [potatoTortilla, "Potato Tortilla", 30, "Ve"], [breadPizza,"Bread Pizza", 15, "Ve" ]]
+
+
